7def49d4bd0f896710687b4d08f3a14dc15ebaaa
sukumidev <usagianonimo@gmail.com>
2024-08-08 13:24:57 -0600

    Added backend and frontend to repository

--- src/routes.py
+++
@@ -1,6 +0,0 @@
-from .resources import CreateUser, Login, Index
-
-def initialize_routes(api):
-    api.add_resource(CreateUser, '/create_user')
-    api.add_resource(Login, '/login')
-    api.add_resource(Index, '/')
